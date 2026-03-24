2305770f7a43f6e7fc51dc566f98be2b39355fab
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-03-24 23:42:28 +0530

    feat: create test_suite
    created the test_suite folder.
    
    added the test for shipClass class

---
+++ test_suite/shipCass.test.js
@@ -0,0 +1,41 @@
+import { test } from "@jest/globals";
+import { expect } from "@jest/globals";
+
+test('check if the length property works',()=>{
+    const ship = new shipClass();
+    expect(ship.length).toBe(undefined);
+    ship.length = 3;
+    expect(ship.length).toBe(3);
+})
+test('check if the hitCount property works',()=>{
+    const ship = new shipClass();
+    expect(ship.hitCount).toBe(0);
+})
+test('check if the isSunk property works',()=>{
+    const ship = new shipClass();
+    expect(ship.isSunk).toBeFalsy();
+    ship.isSunk = true;
+    expect(ship.isSunk).toBeTruthy();
+})
+test('check if the hit method works',()=>{
+    const ship = new shipClass();
+    ship.length = 3;
+    ship.hit()
+    expect(ship.hitCount).toBe(1);
+    ship.hit()
+    expect(ship.hitCount).toBe(2);
+    ship.hit()
+    expect(ship.hitCount).toBe(3);
+})
+test('check if the isSunk method works',()=>{
+    const ship = new shipClass();
+    ship.length = 2;
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
+
+    ship.length = 2;
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+})
