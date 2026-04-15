8c128b7f73e39547483c99a7470d5e6aaa559119
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-04-15 19:19:03 +0530

    test: create tests for player class
    created two tests to check if the type property works properly

---
+++ test_suite/Player.test.js
@@ -0,0 +1,11 @@
+import { test } from "@jest/globals";
+import { expect } from "@jest/globals";
+
+test('Check player types 1', ()=>{
+    const player1 = new Player('human');
+    expect(player1.type).toBe('human');
+})
+test('Check player types 2', ()=>{
+    const player1 = new Player('Computer');
+    expect(player1.type).toBe('Computer');
+})
