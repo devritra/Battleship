85e9427a922d8b69b8a40f410ca3e9245c8a20b3
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-04-15 20:09:48 +0530

    feat: create initGameboard method

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,23 @@
+import { Gameboard } from "../src/Gameboard";
+
 export class Player{
     constructor(type){
         this.type = type;
+        this.gameboard = this.initGameboard();
+    }
+
+    initGameboard(){
+        const gameboard = new Gameboard();
+        gameboard.placeCarrier([9,5],'right');
+        gameboard.placeBattleship([5,1],'down');
+        gameboard.placeBattleship([3,5],'right');
+        gameboard.placeDestroyer([7,7],'right');
+        gameboard.placeSubmarine([2,1],'right');
+        gameboard.placeSubmarine([5,4],'down');
+        gameboard.placePatrolBoat([0,0],'right')
+        gameboard.placePatrolBoat([0,7],'right')
+        gameboard.placePatrolBoat([0,5],'down')
+        gameboard.placePatrolBoat([5,6],'down');
+        return gameboard.grid;
     }
 }
--- test_suite/Player.test.js
+++ test_suite/Player.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-sparse-arrays */
 import { test } from "@jest/globals";
 import { expect } from "@jest/globals";
 import { Player } from "../src/Player";
@@ -9,4 +10,21 @@ test('Check player types 1', ()=>{
 test('Check player types 2', ()=>{
     const player1 = new Player('Computer');
     expect(player1.type).toBe('Computer');
+})
+test('Check player types 2', ()=>{
+    const player1 = new Player('human');
+    expect(player1.gameboard).toEqual(
+        [
+            ['p','p',,,,'p',,'p','p',],
+            [,,,,,'p',,,,],
+            [,'s','s','s',,,,,,],
+            [,,,,,'b','b','b','b',],
+            [,,,,,,,,,],
+            [,'b',,,'s',,'p',,,],
+            [,'b',,,'s',,'p',,,],
+            [,'b',,,'s',,,'d','d','d'],
+            [,'b',,,,,,,,],
+            [,,,,,'c','c','c','c','c']        
+        ]
+    )
 })
