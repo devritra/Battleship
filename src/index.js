8ca7213a4766e2ff303aa46545ae69310250c30c
Aritra Saha <158503009+devritra@users.noreply.github.com>
2026-04-19 19:32:59 +0530

    feat: render player1 grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import { loadGame } from './loadGame';
+import { Player } from './Player';
 const startGameBtn = document.querySelector('.start-game-btn');
 startGameBtn.addEventListener('click',()=>{
-    loadGame();
+    const player1 = new Player('human');
+    const player2 = new Player('computer');
+    loadGame(player1, player2);
 })
--- src/loadGame.js
+++ src/loadGame.js
@@ -1,4 +1,4 @@
-export function loadGame(){
+export function loadGame(player1, player2){
     const gameBox = document.querySelector('.game-box');
     gameBox.innerHTML = '';
     gameBox.classList.remove('home');
@@ -13,4 +13,44 @@ export function loadGame(){
     errorText.classList.add('error-text');
     errorText.textContent = 'error text';
     gameBox.appendChild(errorText);
+
+    const player1Name = document.createElement('p');
+    player1Name.classList.add('player1-name');
+    player1Name.textContent = 'player 1';
+    gameBox.appendChild(player1Name);
+
+    const player2Name = document.createElement('p');
+    player2Name.classList.add('player2-name');
+    player2Name.textContent = 'player 2';
+    gameBox.appendChild(player2Name);
+
+    const player1grid = document.createElement('div');
+    player1grid.classList.add('player1-grid');
+    for(let i = 0; i < player1.gameboardGrid.length; i++){
+        for(let j = 0; j < player1.gameboardGrid[i].length; j++){
+            const gameboardCell = document.createElement('div');
+            gameboardCell.classList.add('gameboard-cell');
+            switch (player1.gameboardGrid[i][j]) {
+                case 'c':
+                    gameboardCell.textContent = 'c';
+                    break;
+                case 'b':
+                    gameboardCell.textContent = 'b';
+                    break;
+                case 'd':
+                    gameboardCell.textContent = 'd';
+                    break;
+                case 's':
+                    gameboardCell.textContent = 's';
+                    break;
+                case 'p':
+                    gameboardCell.textContent = 'p';
+                    break;
+                default:
+                    break;
+            }
+            player1grid.appendChild(gameboardCell);
+        }
+    }
+    gameBox.appendChild(player1grid);
 }
